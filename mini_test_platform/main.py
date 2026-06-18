229a2a9cd14df8f87eab0f05a4aa8a6daa6bf1c7
Leaf Shi (BEYONDSOFT CONSULTING INC) <v-leafshi@microsoft.com>
2026-06-18 15:13:14 +0800

    Add config

--- mini_test_platform/main.py
+++ mini_test_platform/main.py
@@ -1,12 +1,31 @@
 # main.py
+import sys
+
+from logging import config
+
+from config.config_loader import load_config
 from loader.test_loader import load_tests
 from runner.test_runner import run_tests
 from reporter.html_reporter import generate_report
 
 def main():
-    tests = load_tests("tests/test_cases.json")
+
+    config_file = (
+        sys.argv[1]
+        if len(sys.argv)>1
+        else "config.yaml"
+      )
+
+    config=load_config(config_file)
+    test_case_path = config["test_case_path"]
+    output_file = (
+        f"{config['report']['output_dir']}/"
+        f"{config['report']['report_name']}"
+      )
+
+    tests = load_tests(test_case_path)
     results = run_tests(tests)
-    generate_report(results)
+    generate_report(results, output_file)
 
 if __name__ == "__main__":
     main()
--- mini_test_platform/reporter/html_reporter.py
+++ mini_test_platform/reporter/html_reporter.py
@@ -1,14 +1,17 @@
 # reporter/html_reporter.py
 
-def generate_report(results, output="report.html"):
-    with open(output, "w") as f:
+def generate_report(results, output_file):
+    with open(output_file, "w", encoding="utf-8") as f:
         f.write("<html><body>")
         f.write("<h1>Test Report</h1>")
 
         for r in results:
             color = "green" if r["success"] else "red"
             f.write(f"<p style='color:{color}'>")
-            f.write(f"{r['name']}: {'PASS' if r['success'] else 'FAIL'}")
+
+            status =("PASS" if r["success"] else 'FAIL')
+
+            f.write(f"{r['name']}: {status}")
             f.write("</p>")
 
         f.write("</body></html>")
